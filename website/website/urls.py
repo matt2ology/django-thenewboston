ac70d2d213d5fe315b1e546adabc4a1293dd0350
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-05 23:29:17 -0700

    Added path to the music page

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include  # allows referencing other URLconfs.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('music/', include('music.urls')),
 ]
