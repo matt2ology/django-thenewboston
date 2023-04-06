d46962c46cafa540c98b2cee09882c849da9d576
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-05 23:28:36 -0700

    Added content for Music app homepage

--- website/music/views.py
+++ website/music/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse  # basic HTML code or a redirect.
 
-# Create your views here.
+
+def index(request):
+    """Returns the homepage for the Music app.
+
+    Args:
+        request (_type_): this is always the first argument of any view function.
+
+    Returns:
+        _type_: HttpResponse object.
+    """
+    return HttpResponse("<h1>This is the Music app homepage.</h1>")
