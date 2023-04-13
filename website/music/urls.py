4d97c5c3a653691ccf9f6c5225ac8bcc952777ce
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-12 19:06:00 -0700

    Added a new views for the album_id

--- website/music/urls.py
+++ website/music/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     # path('', views.index, name='index'), # the homepage for the Music app.
-    re_path(r'^$', views.index, name='index'),
+    re_path(r'^$', views.index, name='index'), # pattern : /music/
     # q: how do you do path with regex?
     # a: https://docs.djangoproject.com/en/4.2/topics/http/urls/#example
+    path('<int:album_id>/', views.detail, name='detail'), # pattern : /music/album_id/
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -11,3 +11,16 @@ def index(request):
         _type_: HttpResponse object.
     """
     return HttpResponse("<h1>This is the Music app homepage.</h1>")
+
+
+def detail(request: any, album_id: int) -> HttpResponse:
+    """ Returns the details for the album with the given album_id.
+
+    Args:
+        request (_type_): this is always the first argument of any view function.
+        album_id (int): the id of the album.
+
+    Returns:
+        _type_: HttpResponse object.
+    """
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
