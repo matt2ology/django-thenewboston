7c3bcb7676af4a7f326833c124d9aa090ab5955d
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-12 20:52:56 -0700

    Connected database to view

--- website/music/views.py
+++ website/music/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse  # basic HTML code or a redirect.
+from .models import Album  # import the Album model from the models.py file.
 
 
-def index(request):
-    """Returns the homepage for the Music app.
+def index(request: any) -> HttpResponse:
+    """ Returns the index page for the Music app.
 
     Args:
         request (_type_): this is always the first argument of any view function.
@@ -10,7 +11,15 @@ def index(request):
     Returns:
         _type_: HttpResponse object.
     """
-    return HttpResponse("<h1>This is the Music app homepage.</h1>")
+    ALL_ALBUMS: object = Album.objects.all()
+    HTML: str = ''
+    for album in ALL_ALBUMS:
+        # URL pattern: /music/album_id/
+        URL = '/music/' + str(album.id) + '/'
+        # HTML <a> href Attribute
+        # <a href="https://www.w3schools.com">Visit W3Schools</a>
+        HTML += 'Album Title :<a href="' + URL + '">' + album.album_title + '</a><br>'
+    return HttpResponse(HTML)
 
 
 def detail(request: any, album_id: int) -> HttpResponse:
