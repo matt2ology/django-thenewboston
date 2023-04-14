65f5a6bf1575df777de55faf6f991ddf72769d43
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-14 10:09:22 -0700

    Added the songs table to be accessed in admin page

--- website/music/admin.py
+++ website/music/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-# we need this so we can register the Album model with the admin site
-from .models import Album
+# we need this so we can register the Album and Song model with the admin site
+from .models import Album, Song
 
 # this is how we register the Album model with the admin site so we can see it in the admin site
 admin.site.register(Album)
+admin.site.register(Song)
