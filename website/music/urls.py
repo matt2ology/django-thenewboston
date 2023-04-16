96d2a223360f389d6fc3e17f1fdf95a2c39e2356
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-16 00:19:29 -0700

    Formatted document

--- website/music/urls.py
+++ website/music/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, re_path
 from . import views
 
-app_name : str = 'music'  # this is the namespace for the Music app.
+app_name: str = 'music'  # this is the namespace for the Music app.
 
 urlpatterns = [
     # path('', views.index, name='index'), # the homepage for the Music app.
-    re_path(r'^$', views.index, name='index'), # pattern : /music/
+    re_path(r'^$', views.index, name='index'),  # pattern : /music/
     # q: how do you do path with regex?
     # a: https://docs.djangoproject.com/en/4.2/topics/http/urls/#example
     path('<int:album_id>/', views.detail, name='detail'), # pattern : /music/album_id/
