11e8de9f92be50b8b757210bc4ab9977f074373d
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-16 00:20:16 -0700

    Added a function that allows users to favorite tracks in Album

--- website/music/views.py
+++ website/music/views.py
@@ -1,4 +1,4 @@
-from .models import Album  # import the Album model from the models.py file.
+from .models import Album, Song  # import the Album and Song models.
 
 from django.http import HttpResponse  # basic HTML code or a redirect.
 from django.shortcuts import render, get_object_or_404  # render a template.
@@ -33,3 +33,28 @@ def detail(request: any, album_id: int) -> HttpResponse:
     ALBUM = get_object_or_404(
         Album, pk=album_id)  # removes the need for try/except block.
     return render(request, 'music/detail.html', {'album': ALBUM})
+
+
+def favorite(request: any, album_id: int) -> HttpResponse:
+    """ Returns the detail page for the Music app.
+
+    Args:
+        request (_type_): this is always the first argument of any view function.
+        album_id (_type_): the id of the album.
+
+    Returns:
+        _type_: HttpResponse object.
+    """
+    ALBUM = get_object_or_404(Album, pk=album_id)
+    try:
+        # get the song id from the POST request from the detail page form.
+        SELECTED_SONG = ALBUM.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': ALBUM,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        SELECTED_SONG.is_favorite: bool = True
+        SELECTED_SONG.save()
+        return render(request, 'music/detail.html', {'album': ALBUM})
